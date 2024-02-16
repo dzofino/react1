e0c0edf2ff9150c5438344271130999c3e35228b
dzofino <sofien.khaterchi1982@gmail.com>
2024-02-16 09:54:49 +0100

    react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Naviguation from './components/Naviguation';
+import Cards from './components/Cards';
+
+function App() {
+  let name = Blob
+
+  return (
+    <div className="App">
+    <Naviguation/>
+      <h1>My {name} is </h1>
+    <Cards/>
+
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Cards.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+import "./cards.css"
+
+function Cards() {
+  return (
+    <div style={{display:"flex", justifyContent:"center",margin:"10%"}}>
+        <Card border="info" style={{ width: '18rem' }}>
+        <Card.Header>Header</Card.Header>
+        <Card.Body>
+          <Card.Title>Info Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card border="info" style={{ width: '18rem' }}>
+        <Card.Header>Header</Card.Header>
+        <Card.Body>
+          <Card.Title>Info Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card border="info" style={{ width: '18rem' }}>
+        <Card.Header>Header</Card.Header>
+        <Card.Body>
+          <Card.Title>Info Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  )
+}
+
+export default Cards
